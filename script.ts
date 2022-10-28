84a5e739cbd7d2efd6a59b0bba30e1e0538af52b
dankorotenko <danilkorotenko2@gmail.com>
2022-10-29 01:23:52 +0200

    prettier

--- script.ts
+++ script.ts
@@ -1,13 +1,13 @@
-const cards = document.querySelectorAll<HTMLElement>('.card');
-const cardWrapper = document.getElementById('cards') as HTMLDivElement;
+const cards = document.querySelectorAll<HTMLElement>(".card");
+const cardWrapper = document.getElementById("cards") as HTMLDivElement;
 
-cardWrapper.onmousemove = e => {
-    cards.forEach((card) => {
-        const rect = card.getBoundingClientRect(),
-            x = e.clientX - rect.left,
-            y = e.clientY - rect.top;
+cardWrapper.onmousemove = (e) => {
+  cards.forEach((card) => {
+    const rect = card.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top;
 
-        card.style.setProperty("--mouse-x", `${x}px`);
-        card.style.setProperty("--mouse-y", `${y}px`);
-    })
-}
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  });
+};
